4c381341d1ed598b3a471366e3f426f6e63ffc7a
abbasinima <n.abbasi7@yahoo.com>
2017-06-14 15:23:06 -0400

    first commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import os
 from bokeh.embed import components
 import pandas as pd
-from bokeh.models import HoverTool
+#from bokeh.models import HoverTool
 
 app = Flask(__name__)
 
@@ -42,9 +42,8 @@ def result():
     df1['date']=pd.to_datetime(df1['date'])
     df1=df1.sort_values(by='date',ascending=False)
     
-    hover = HoverTool(tooltips=[('Date', '$x')])
-    plot=figure(tools=[hover],#tools="pan,wheel_zoom,box_zoom,reset,save",
-            
+    
+    plot=figure(tools="pan,wheel_zoom,box_zoom,reset,save",
                 title='Data from Quandle WIKI set',
                 x_axis_label='date',
                 x_axis_type='datetime')
